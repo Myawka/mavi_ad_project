ceba2409a1bfb8913169830260d3be395084cf49
myawka <ssscolllo@gmail.com>
2025-05-21 17:52:17 +0300

    5.5. Подключение данных к grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,23 +17,23 @@
       </v-col>
     </v-row>
   </v-container>
-  <v-container>
+  <v-container grid-list-lg>
     <v-row justify="center">
-      <v-col cols="12" xs="12">
+      <v-col cols="3" sm="3" md="3" lg="3" xs="12" 
+      v-for="ad in ads" 
+      :key="ad.id">
         <v-card>
-          <v-img height="200px"></v-img>
+           <v-img :src="ad.src" height="200px"></v-img>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb0">Здесь будет заголовок</h3>
-              <div>Здесь будет описание</div>
+              <h3 class="headline mb0">{{ ad.title }}</h3>
+              <div>{{ ad.desc }}</div>
             </div>
           </v-card-title>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn text>Open</v-btn>
-            <v-btn raised color="primary">
-              Buy
-            </v-btn>
+            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+            <v-btn raised color="primary">Buy</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -68,7 +68,7 @@ data () {
           id: "3"
         },
         {
-          title: "Fouth",
+          title: "Fourth",
           desc: "Fouth Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
@@ -81,14 +81,14 @@ data () {
 </script>
 
 <style scoped>
-	.ad-link {
-		position:absolute;
-		bottom: 50px;
-		left:50%;
-		background: rgb(0,0,0,0.5);
-		transform: translate(-50%,0);
-		padding: 5px 15px;
-		border-top-right-radius: 5px;
-		border-top-left-radius: 5px;
-	}
+	..ad-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgb(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
 </style>
