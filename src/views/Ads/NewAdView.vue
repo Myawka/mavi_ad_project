4e4cade64065c023f2bf436efbb528c06fb85142
myawka <ssscolllo@gmail.com>
2025-05-25 14:54:14 +0300

    11.1. Подготовка данных объявления

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -57,7 +57,8 @@ data () {
 	const ad = {
 		title: this.title,
 		desc: this.description,
-		promo: this.promo
+		promo: this.promo,
+    src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
 	}
 	console.log(ad)
 	}
