3dbd49c4e1f4e8ab7fd681eb81b8f4ec975045f9
myawka <ssscolllo@gmail.com>
2025-05-25 14:41:38 +0300

    10.4. Подключение объявлений из store

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -3,7 +3,10 @@
     <v-row justify="center">
       <v-col cols="12" sm="8" lg="6">
         <h1 class="text--secondary mb-3 mt-3">My ads</h1>
-        <v-card v-for="ad in ads" :key="ad.id" class="mb-3" max-width="1000">
+        <v-card 
+	class="elevation-10 mb-5"
+	v-for="ad in myAds"
+	:key="ad.id">
           <v-row>
               <v-col xs="4">
                 <v-img :src="ad.src" height="175px" cover></v-img>
@@ -25,40 +28,10 @@
 
 <script>
 export default {
-data () {
-  return {
-    ads:[
-	{
-		title:"First",
-		desc:"First Desc",
-		promo: true,
-src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-		id:"1"
-	},
-	{
-		title:"Second",
-		desc:"Second Desc",
-		promo: true,
-		src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-		id:"2"
-	},
-	{
-		title:"Third",
-		desc:"Thitd Desc",
-		promo: true,
-		src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-		id:"3"
-	},
-	{
-		title:"Fourth",
-		desc:"Fourth Desc",
-		promo: true,
-		src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-		id:"4"
+computed: {
+	myAds(){
+	return this.$store.getters.myAds
 	}
-	]
-} 	
-
-  }
+}
 }
 </script>
