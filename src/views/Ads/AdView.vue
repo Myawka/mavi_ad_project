4bdc9a8bda6bfd524b68f8877492fe74d39fd8ef
myawka <ssscolllo@gmail.com>
2025-05-25 14:50:44 +0300

    10.5. Просмотр деталей объявления

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -5,13 +5,9 @@
         <v-card class="mt-5">
            <v-img height="300px" src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"></v-img>
           <v-card-text>
-            <h1 class="text--primary mb-3">Lorem Ipsum</h1>
+            <h1 class="text--primary mb-3">{{ ad.title }}</h1>
             <p>
-              Lorem Ipsum has been the industry's unknown printer took a galley of type and scrambled it to make a type
-              specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-              including versions of Lorem Ipsum.
+              {{ ad.desc}}
             </p>
           </v-card-text>
           <v-card-actions>
@@ -28,9 +24,12 @@
 
 <script>
 export default {
-data () {
-  return {
+  props: ['id'],
+    computed: {
+		ad() {
+		const id = this.id
+		return this.$store.getters.adById(id)
+		}
+	}
     }
-  }
-}
 </script>
