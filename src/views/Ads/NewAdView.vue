de0ff2c16f834ba6a54cbd8859f41c042df67eff
myawka <ssscolllo@gmail.com>
2025-06-19 18:15:13 +0300

    16.3. Подключение лоудера на экране создания объявления

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -35,7 +35,13 @@
         <v-row>
           <v-col cols="8">
             <v-spacer></v-spacer>
-            <v-btn color="success" @click="createAd" :loading="loading" :disabled:="!valid || loading">Create Ad</v-btn>
+            <v-btn
+            color="success"
+            @click="createAd"
+            :loading="loading"
+            :disabled:="!valid || loading"
+            >Create Ad</v-btn>
+
           </v-col>
         </v-row>
     
@@ -62,7 +68,12 @@ data () {
 	}
   this.$store.dispatch("createAd", ad)
 	}
-    }
-  }
+    },
+    computed: {
+        loading() {
+            return this.$store.getters.loading
+        }
+    },
+}
 }
 </script>
