43cd4490c3edd7de6a538d1e688cfc304f569ca4
myawka <ssscolllo@gmail.com>
2025-06-19 09:46:27 +0300

    13.5. Редирект после успешного создания пользователя

--- src/views/Auth/RegistrationView.vue
+++ src/views/Auth/RegistrationView.vue
@@ -82,6 +82,13 @@ data () {
 				password: this.password
 			}
 			this.$store.dispatch('registerUser', user)
+      .then(() => {
+        this.$router.push("/")
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+
 		}
         }
       }
