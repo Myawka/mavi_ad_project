3ca53ed8d11a8a14df05dcc2b554b8b3d9d78273
myawka <ssscolllo@gmail.com>
2025-06-19 16:05:46 +0300

    15.2. Управление пунктами меню для залогиненного или незалогиненного пользователя

--- src/App.vue
+++ src/App.vue
@@ -76,6 +76,23 @@ export default {
       return this.$store.getters.error
     }
   },
+  isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn
+    },
+    links() {
+      if (this.isUserLoggedIn) {
+        return [
+          { title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders" },
+          { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+          { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
+        ]
+      } else {
+        return [
+          { title: "Login", icon: "mdi-lock", url: "/login" },
+          { title: "Registration", icon: "mdi-face", url: "/registration" },
+        ]
+      }
+    },
   methods: {
     closeError() {
       this.$store.dispatch('clearError')
