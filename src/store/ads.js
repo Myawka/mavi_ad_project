1ab98e6712be888883cd7b77315f5f77da37c0b9
myawka <ssscolllo@gmail.com>
2025-06-19 18:55:30 +0300

    16.4. Логика отображений объявлений пользователя

--- src/store/ads.js
+++ src/store/ads.js
@@ -74,9 +74,12 @@ export default {
           return ad.promo;
         });
       },
-      myAds(state) {
-        return state.ads;
+      myAds(state, getters) {
+        return state.ads.filter(ad => {
+        return ad.userId == getters.user.id
+        });
       },
+
       adById(state) {
         return id => {
         return state.ads.find(ad => ad.id == id)
