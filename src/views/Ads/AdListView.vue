8df35af18810a7912dd893d52ae2b2c37a9b212a
myawka <ssscolllo@gmail.com>
2025-06-19 19:32:12 +0300

    17.4. Логика редактирования

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -4,21 +4,24 @@
       <v-col cols="12" sm="8" lg="6">
         <h1 class="text--secondary mb-3 mt-3">My ads</h1>
         <v-card 
-	class="elevation-10 mb-5"
-	v-for="ad in myAds"
-	:key="ad.id">
+          class="elevation-10 mb-5"
+          v-for="ad in myAds"
+          :key="ad.id"
+        >
           <v-row>
-              <v-col xs="4">
-                <v-img :src="ad.src" height="175px" cover></v-img>
-              </v-col>
-              <v-col xs="8">
-                  <h2 class="text--primary">{{ ad.title }}</h2>
-                  <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                      <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">Open</v-btn>
-                  </v-card-actions>
-              </v-col>
+            <v-col xs="4">
+              <v-img :src="ad.src" height="175px" cover></v-img>
+            </v-col>
+            <v-col xs="8">
+              <h2 class="text--primary">{{ ad.title }}</h2>
+              <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">Open</v-btn>
+                <!-- Добавляем компонент EditAdModal для каждого объявления -->
+                <EditAdModal :ad="ad" />
+              </v-card-actions>
+            </v-col>
           </v-row>
         </v-card>
       </v-col>
@@ -27,11 +30,16 @@
 </template>
 
 <script>
+import EditAdModal from './EditAdModal.vue'; 
+
 export default {
-computed: {
-	myAds(){
-	return this.$store.getters.myAds
-	}
-}
-}
+  components: {
+    EditAdModal, 
+  },
+  computed: {
+    myAds() {
+      return this.$store.getters.myAds;
+    },
+  },
+};
 </script>
