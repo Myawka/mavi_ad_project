82293cbba209defc865e8417818a5a1ed4cdd1be
myawka <ssscolllo@gmail.com>
2025-06-19 16:40:26 +0300

    15.4. Защита роутера

--- src/store/user.js
+++ src/store/user.js
@@ -41,13 +41,13 @@ export default {
     },
     async loginUser({ commit }, { email, password }) {
       commit("clearError");
-      commit("setLoading", true);
+      commit("setLoading", false);
       //Здесь выполняется запрос на сервер
       let isRequestOk = true;
       let promise = new Promise(function (resolve) {
         setTimeout(() => resolve("Done"), 3000);
       });
-      
+
       if (isRequestOk) {
         await promise.then(() => {
           commit("setUser", new User(1, email, password));
@@ -55,7 +55,7 @@ export default {
         });
       } else {
         await promise.then(() => {
-          commit("setLoading", true);
+          commit("setLoading", false);
           commit("setError", "Ошибка логина или пароля");
           throw "Упс... Ошибка логина или пароля";
         });
