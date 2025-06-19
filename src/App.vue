f2990c8adfe5c70e9c3cd70ed052ddf394883252
myawka <ssscolllo@gmail.com>
2025-06-19 17:49:55 +0300

    16.1.  Добавление userId в сущность объявления

--- src/App.vue
+++ src/App.vue
@@ -3,46 +3,47 @@
     <v-navigation-drawer app v-model="drawer">
 
       <v-list-item>
-      <v-list-item-content>
-            <v-list-item-title class="title">
-              КИПУ
-            </v-list-item-title>
-                <v-list-item-subtitle>
-              Учебный проект
-            </v-list-item-subtitle>
-      </v-list-item-content>
-  </v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-  <v-divider></v-divider>
+      <v-divider></v-divider>
 
-   <v-list dense>
+      <v-list dense>
 
-        <v-list-item
-          v-for="link in links"
-          :key="link.title"
-          :to="link.url"
-          >
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
-          <v-icon :icon="link.icon"></v-icon>
+            <v-icon :icon="link.icon"></v-icon>
           </template>
           <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
-
-
       </v-list>
-        <v-list-item @click="onLogout" v-if="isUserLoggedIn">
-          <template v-slot:prepend>
-            <v-icon icon="mdi-exit-to-app"></v-icon>
-          </template>
-          <v-list-item-title>Logout</v-list-item-title>
-        </v-list-item>
 
+      <v-list-item @click="onLogout" v-if="isUserLoggedIn">
+        <template v-slot:prepend>
+          <v-icon icon="mdi-exit-to-app"></v-icon>
+        </template>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
 
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <v-btn to="/">
+          Home
+        </v-btn>
+      </v-toolbar-title>
+
       <v-spacer></v-spacer>
-          <v-toolbar-items class="hidden-sm-and-down">
+      <v-toolbar-items class="hidden-sm-and-down">
         <v-btn v-for="link in links" :key="link.title" :to="link.url">
           <v-icon start :icon="link.icon"></v-icon>
           {{ link.title }}
@@ -53,11 +54,12 @@
           Logout
         </v-btn>
 
-      </v-toolbar-items> 
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <router-view></router-view>
     </v-main>
+
     <v-snackbar v-model="error" multi-line :timeout="2000" color="primary">
       {{ error }}
       <template v-slot:actions>
@@ -74,23 +76,23 @@ export default {
   data() {
     return {
       drawer: false,
-         links: [
-     {title:"Login", icon:"mdi-lock", url:"/login"},
-        {title:"Registration",icon:"mdi-face",url:"/registration"},
-        {title:"Orders",icon:"mdi-bookmark-multiple-outline",
-          url:"/orders"},
-     {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
-     {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
-      ]
-
+      // links: [
+      //   { title: "Login", icon: "mdi-lock", url: "/login" },
+      //   { title: "Registration", icon: "mdi-face", url: "/registration" },
+      //   {
+      //     title: "Orders", icon: "mdi-bookmark-multiple-outline",
+      //     url: "/orders"
+      //   },
+      //   { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+      //   { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
+      // ]
     }
   },
   computed: {
     error() {
       return this.$store.getters.error
-    }
-  },
-  isUserLoggedIn() {
+    },
+    isUserLoggedIn() {
       return this.$store.getters.isUserLoggedIn
     },
     links() {
@@ -107,6 +109,7 @@ export default {
         ]
       }
     },
+  },
   methods: {
     closeError() {
       this.$store.dispatch('clearError')
@@ -115,6 +118,8 @@ export default {
       this.$store.dispatch('logoutUser')
       this.$router.push("/")
     }
+
   }
+
 }
 </script>
